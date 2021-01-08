1b283bb039ba72464771940d872a87ecc009f15d
Chua Bing Quan <chipneverdies@gmail.com>
2021-01-08 11:06:10 +0800

    Initialise server

--- cmd/quartz/main.go
+++ cmd/quartz/main.go
@@ -1,3 +1,69 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+	"quartz"
+	"quartz/http"
+	"quartz/postgres"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	config, err := getConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := postgres.Open(config.DatabaseURL)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	js := &postgres.JobService{DB: db}
+
+	server := http.Server{
+		Port:       config.Port,
+		Router:     gin.Default(),
+		JobService: js,
+	}
+	server.Start()
+}
+
+func getConfig() (quartz.Config, error) {
+	const (
+		envPort        = "PORT"
+		envDatabaseURL = "DATABASE_URL"
+	)
+
+	envNames := []string{
+		envPort,
+		envDatabaseURL,
+	}
+
+	values := map[string]string{}
+	for _, envName := range envNames {
+		val, ok := os.LookupEnv(envName)
+		if !ok {
+			return quartz.Config{}, fmt.Errorf("\"%s\" environment variable is required but not set", envName)
+		}
+		values[envName] = val
+	}
+
+	return quartz.Config{
+		Port:        toIntOrPanic(values[envPort]),
+		DatabaseURL: values[envDatabaseURL],
+	}, nil
+}
+
+func toIntOrPanic(s string) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		panic(fmt.Errorf("Failed to parse \"%s\" to int: %w", s, err))
+	}
+	return val
+}
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/google/uuid v1.1.4 // indirect
 	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/lib/pq v1.9.0 // indirect
 )
